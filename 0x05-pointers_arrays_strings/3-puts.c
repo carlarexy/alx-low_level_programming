e6d0a6ffdff8c217f3e7164aecfc17a0f12b7cb7
carlarexy <carlanjeri17@gmail.com>
2023-09-18 09:58:28 -0700

    puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
-int putchar(char c);
 
 /**
  * _puts - Prints a string
  * @str: The string to print
  * Return: void
  */
+
 void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
-	_putchar(*str);
+	putchar(*str);
 	}
 	_putchar('\n');
 }
