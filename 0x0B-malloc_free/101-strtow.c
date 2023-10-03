102d97b9b2e82afbe8249e63e0147e8d468bb01f
carlarexy <carlanjeri17@gmail.com>
2023-10-03 09:17:24 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,7 +10,7 @@ char **strtow(char *str);
  *            first word contained within a string.
  * @str: The string to be searched.
  *
- * Return: The index marking the end of the initial word pointed to bystr.
+ * Return: The index marking the end of the initial word pointed to by str.
  */
 int word_len(char *str)
 {
@@ -26,7 +26,7 @@ int word_len(char *str)
 }
 
 /**
- * count_words - counts the number of words contained within a string.
+ * count_words - Counts the number of words contained within a string.
  * @str: The string to be searched.
  *
  * Return: The number of words contained within str.
@@ -48,4 +48,55 @@ int count_words(char *str)
 	}
 
 	return (words);
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The string to the split>
+ *
+ * Return: If str = NULL, str = "", or the function fails - NULL.
+ *         Otherwise - a pointer to an array of strings (words).
+ */
+char **strtow(char *str)
+{
+	char **strings;
+	int index = 0, words, w, letters, l;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
+
+	for (w = 0; w < words; w++)
+	{
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_len(str + index);
+
+		strings[w] = malloc(sizeof(char) * (letters + 1));
+
+		if (strings[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(strings[w]);
+
+			free(strings);
+			return (NULL);
+		}
+
+		for (l = 0; l < letters; l++)
+			strings[w][l] = str[index++];
+
+		strings[w][l] = '\0';
+	}
+	strings[w] = NULL;
+
+	return (strings);
 }
